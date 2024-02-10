65fc7fde3fba14e973c59d8b543e2c41280eac60
Ricardo Boraure <ricardoboraure350@gmail.com>
2024-02-10 14:59:16 -0400

    feat: Add icons

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,9 +1,15 @@
-import { View, Text } from "react-native";
+import { H1, View, XStack, Button } from "tamagui";
+import { Bell } from "@tamagui/lucide-icons";
 
 export function Header() {
   return (
-    <View>
-      <Text>Header</Text>
+    <View paddingTop="$4" paddingHorizontal="$4">
+      <XStack justifyContent="space-between" alignItems="center">
+        <H1 fontSize="$8" textAlign="center">
+          Domicilios Pet RD
+        </H1>
+        <Button icon={Bell}></Button>
+      </XStack>
     </View>
   );
 }
